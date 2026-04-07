6337ac0bec751686976e6ba875b1e1bdc9264a9e
root <root@srv1080107.hstgr.cloud>
2026-04-07 14:18:45 +0000

    correcao erro aws senha

--- infrastructure/aws/ec2/main.tf
+++ infrastructure/aws/ec2/main.tf
@@ -1,10 +1,15 @@
-variable "aws" {
-  type = string
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type      = string
   sensitive = true
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
