a83755b819f806068f682cceae6c1e60165df36a
root <root@srv1080107.hstgr.cloud>
2026-04-07 14:09:00 +0000

    fix aws credenciais

--- infrastructure/aws/ec2/main.tf
+++ infrastructure/aws/ec2/main.tf
@@ -1,5 +1,12 @@
+variable "aws" {
+  type = string
+  sensitive = true
+}
+
 provider "aws" {
   region = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "flux_lab" {
